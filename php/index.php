efc605104d5870cd308cb9098698f70555b2d019
Maykon Dias <Maykondias2001@gmail.com>
2022-12-21 23:12:13 -0300

    construindo layout v0.1

---
+++ php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/View/reset.css">
+    <link rel="stylesheet" href="/View/style.css">
+    <title>Estudo PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo">
+                    <h3>Teste</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php">
+                                Exemplo
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        MDS PROJECTS © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
